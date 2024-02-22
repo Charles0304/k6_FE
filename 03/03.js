65c04c219202ecf1fa5513d58f9015267a7f5474
Charles0304 <cotncjf2@gmail.com>
2024-02-22 13:01:25 +0900

    0222

--- 03/03.js
+++ 03/03.js
@@ -1,5 +1,9 @@
-function handleClick(n){
+/*function handleClick(n){
     // document.getElementById("msgArea").innerHTML = "안녕하세요";
     document.querySelector("#msgArea").innerHTML="<h2>버튼"+n+"이 눌러졌습니다.</h2>"
+    document.querySelector("#msgArea").innerHTML=`<h2>버튼 ${n} 이 눌러졌습니다.</h2>`
+}*/
+
+const handleClick = (n) =>{
     document.querySelector("#msgArea").innerHTML=`<h2>버튼 ${n} 이 눌러졌습니다.</h2>`
 }
